4b6d800127b98788f073a7dfa56709fe2c9e26e0
merlynjwa <merlyn.wa@gmail.com>
2023-03-24 02:24:55 +0000

    Add files produced by running 'python3 manage.py startapp restaurant_booking_system'

---
+++ restaurant_booking_system/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RestaurantBookingSystemConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'restaurant_booking_system'
