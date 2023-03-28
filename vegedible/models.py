8f27f196c42026ee1cc508fd2bb59175932eea1e
merlynjwa <merlyn.wa@gmail.com>
2023-03-28 00:48:33 +0000

    Add a 'Customers' model
    The model has two fields.
    
    An email address field as a primary key which uses Django's email verifier.
    
    A CharField for the customer's name.
    I might separate the field into 'first_name' and 'last_name',
    but I need to figure out a way to combine them in the __str__ method
    so that the full name is returned.
    
    I've also implemented a __str__ method which returns the name field.

--- vegedible/models.py
+++ vegedible/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customers(models.Model):
+    email = models.EmailField(primary_key=True)
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
