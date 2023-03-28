b4d0e339a9c223120edf44b82a6be5a67d95b5e1
merlynjwa <merlyn.wa@gmail.com>
2023-03-28 00:54:31 +0000

    Rename the 'Customers' model to 'Customer'
    I realised that the admin page for Django automatically pluralises
    the name of models for the tables.
    
    It also is a better name from a type perspective, as the class which
    represents the customer does not model multiple customers in of itself,
    it represents a single instance of a customer.

--- vegedible/models.py
+++ vegedible/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class Customers(models.Model):
+class Customer(models.Model):
     email = models.EmailField(primary_key=True)
     name = models.CharField(max_length=30)
 
