4491f2da7b253bed6f5c191448c18933fca04ada
merlynjwa <merlyn.wa@gmail.com>
2023-03-29 18:28:09 +0000

    Change the django user model to my 'Customer' model
    I decided that I didn't want the user to have to go through two
    separate stages for creating an account, one for a user account,
    created using django's user model, and another for the customers
    table to add their orders against.
    
    So, I made django use my 'Customer' model as the user model.
    
    In doing this, I've removed the 'user' field from the model and
    added a 'username' field, which just accepts a string.
    
    I've configured the 'first_name', 'last_name' and 'email' fields to
    be required by a new record in the table.
    
    I also removed the previous migrations and re-created the migrated
    django code; just to avoid any nasty issues with migration.

--- vegedible/admin.py
+++ vegedible/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Customer, Order
 
 # Register your models here.
 
-admin.site.register(Customer)
+admin.site.register(Customer, UserAdmin)
 admin.site.register(Order)
--- vegedible/models.py
+++ vegedible/models.py
@@ -1,15 +1,21 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.core import validators
 
 # Create your models here.
 
 
-class Customer(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    email = models.EmailField(primary_key=True)
+class Customer(AbstractUser):
+    username = models.CharField(max_length=40, unique=True)
+    email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=15)
     last_name = models.CharField(max_length=15)
+
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'email']
+
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return '%s (%s %s)' % (self.email,
