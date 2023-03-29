a49e4bff213ed0132391ff0e56e67bdce96517f7
merlynjwa <merlyn.wa@gmail.com>
2023-03-29 00:26:50 +0000

    Add 'user' field to 'Customer' model
    This field uses a django 'User' model.
    This allows the creation of user accounts with credentials and
    permission assignments.

--- vegedible/models.py
+++ vegedible/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.core import validators
 
 # Create your models here.
 
 
 class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     email = models.EmailField(primary_key=True)
     first_name = models.CharField(max_length=15)
     last_name = models.CharField(max_length=15)
