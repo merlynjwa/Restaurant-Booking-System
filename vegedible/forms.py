b10f98f008c78c2d398be54c10bca4cff48e50e7
merlynjwa <merlyn.wa@gmail.com>
2023-04-01 18:12:46 +0000

    Have the 'OrderForm' render the data & time fields

--- vegedible/forms.py
+++ vegedible/forms.py
@@ -1,9 +1,12 @@
 from django import forms
 from .models import Order
+from django.contrib.admin import widgets
 
 
 class OrderForm(forms.ModelForm):
+    date_and_time = forms.SplitDateTimeField(widget=widgets.AdminSplitDateTime)
 
     class Meta:
         model = Order
-        fields = ['table_number']
+        fields = ['table_number',
+                  'date_and_time']
