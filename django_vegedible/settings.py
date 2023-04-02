1a0746298854fb695bf089b93a3ce7f371206655
merlynjwa <merlyn.wa@gmail.com>
2023-04-02 02:22:08 +0000

    Set the application to use an external database
    DATABASES is now set to a privately stored value,
    both so that it uses an external database, and for security.

--- django_vegedible/settings.py
+++ django_vegedible/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
@@ -87,12 +88,8 @@ WSGI_APPLICATION = 'django_vegedible.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
