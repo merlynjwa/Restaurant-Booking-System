073ca69b87c5f6322494d2f37b3241630069f2b9
merlynjwa <merlyn.wa@gmail.com>
2023-04-02 05:35:53 +0000

    Test that the table number is a required field in the order form

--- vegedible/tests.py
+++ vegedible/tests.py
@@ -6,3 +6,9 @@ from .forms import OrderForm
 
 class TestOrderForm(TestCase):
 
+    def test_table_number_is_required(self):
+        form = OrderForm({'table_number': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('table_number', form.errors.keys())
+        self.assertEqual(form.errors['table_number'][0],
+                         'This field is required.')
