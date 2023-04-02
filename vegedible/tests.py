4254f405c00aeaae64a7897f957fe13f73b4d079
merlynjwa <merlyn.wa@gmail.com>
2023-04-02 05:37:22 +0000

    Test that the date and time fields are required in the order form

--- vegedible/tests.py
+++ vegedible/tests.py
@@ -12,3 +12,10 @@ class TestOrderForm(TestCase):
         self.assertIn('table_number', form.errors.keys())
         self.assertEqual(form.errors['table_number'][0],
                          'This field is required.')
+
+    def test_date_and_time_is_required(self):
+        form = OrderForm({'date_and_time': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('date_and_time', form.errors.keys())
+        self.assertEqual(form.errors['date_and_time'][0],
+                         'This field is required.')
