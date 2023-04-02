451c5ba0d487ee3a0bd27517df6eec8e97fbb1a4
merlynjwa <merlyn.wa@gmail.com>
2023-04-02 05:42:39 +0000

    Test that the home page gives a 200 status when requested

--- vegedible/tests.py
+++ vegedible/tests.py
@@ -27,3 +27,7 @@ class TestOrderForm(TestCase):
 
 class TestViews(TestCase):
 
+    def test_get_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'vegedible/home_page.html')
